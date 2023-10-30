0a47c497a47b9bb368d4fb44079af780e8df13df
Belous Danila <atlts228134@gmail.com>
2023-10-30 18:29:02 +0300

    add: create loop.js file

---
+++ loop.js
@@ -0,0 +1,11 @@
+for (let i=0; i<11; i++){
+    console.log(`на ${i} итерации число ${i}`)
+}
+
+let count = 1
+for (let i=9916; i<10000; i++){
+
+    if (i%2!==0)
+    console.log(`на ${count} итерации число ${i}`)
+    count++
+}
