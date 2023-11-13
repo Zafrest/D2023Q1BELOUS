dca59c1cd1434df02abd57fc563dbdc2821cbc7b
Belous Danila <atlts228134@gmail.com>
2023-11-13 18:35:28 +0300

    add:  create file kakaphone.js

--- kakaphone.js
+++ kakaphone.js
@@ -1,4 +1,8 @@
-let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let numb = number.map(num => num * 2);
-
-console.log(numb);
+add = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const result = []
+for (item = 1; item < 10; item++){
+    for(item2 = 1; item2 < 10; item2++){
+        console.table(item * item2)
+    }
+}
+console.table(result)
